d5a5a89483993ce8e2eb0f294e4806b3296c8575
Zack Blackstone <zblackstone11@yahoo.com>
2024-03-30 15:58:38 -0400

    Added a search view controller, next is Photo.java

---
+++ src/viewController/SearchViewController.java
@@ -0,0 +1,7 @@
+package viewController;
+
+// Represents the controller for the search view, which handles searching for photos based on various criteria.
+
+public class SearchViewController {
+    
+}
