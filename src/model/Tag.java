4c07039eb42f0d10961ec998f30dd31153d2f335
Zack Blackstone <zblackstone11@yahoo.com>
2024-03-31 13:48:26 -0400

    User and Tag classes

--- src/model/Tag.java
+++ src/model/Tag.java
@@ -8,6 +8,69 @@ package model;
 //All tags are opitoal but I thinbk we should have a Stock boolean that is necessary for all 
 
 public class Tag {
-    
+
+    // First some fields including the tag name and value.
+    private String tagName; // location, person, aspect ratio etc
+    private String tagValue; // New York, John Doe, 16:9 etc
+
+    // Constructors: one that takes just the tag name, and one that takes both.
+
+    // Constructor that takes just the tag name.
+    public Tag(String tagName) {
+        this.tagName = tagName;
+        this.tagValue = "";
+    }
+
+    // Constructor that takes both.
+    public Tag(String tagName, String tagValue) {
+        this.tagName = tagName;
+        this.tagValue = tagValue;
+    }
+
+    // Getters and setters for all fields.
+
+    // Getter for tag name.
+    public String getTagName() {
+        return tagName;
+    }
+
+    // Setter for tag name.
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
+    // Getter for tag value.
+    public String getTagValue() {
+        return tagValue;
+    }
+
+    // Setter for tag value.
+    public void setTagValue(String tagValue) {
+        this.tagValue = tagValue;
+    }
+
+    // Equals, hashCode, and toString methods.
+
+    // Equals method that checks if two tags are equal.
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Tag)) {
+            return false;
+        }
+        Tag other = (Tag) obj;
+        return tagName.equals(other.tagName) && tagValue.equals(other.tagValue);
+    }
+
+    // Hashcode method.
+    public int hashCode() {
+        return java.util.Objects.hash(tagName, tagValue);
+    }
+
+    // toString method.
+    public String toString() {
+        return tagName + ": " + tagValue;
+    }
     
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,8 +1,162 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Represents a user with properties like username, list of albums, and possibly a password. 
 // Methods might involve album management (add, delete, rename).
 
 public class User {
+
+    // First some fields including the username and a list of albums.
+    private String username;
+    private java.util.ArrayList<Album> albums;
+
+    // Constructor that takes just the username.
+    public User(String username) {
+        this.username = username;
+        this.albums = new java.util.ArrayList<Album>();
+    }
+
+    // Constructor that takes username and albums.
+    public User(String username, java.util.ArrayList<Album> albums) {
+        this.username = username;
+        this.albums = new java.util.ArrayList<>(albums); // make a copy of the list
+    }
+
+    // Getters and setters for all fields.
+
+    // Getter for username.
+    public String getUsername() {
+        return username;
+    }
+
+    // Setter for username.
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter for albums.
+    public java.util.ArrayList<Album> getAlbums() {
+        return albums;
+    }
+
+    // Setter for albums. Might drop this and just use addAlbum/removeAlbum methods.
+    public void setAlbums(java.util.ArrayList<Album> albums) {
+        this.albums = albums;
+    }
+
+    // Methods like create album, delete album, rename album
+
+    // Method to add an album to the list of albums.
+    // Change to check for duplicates since we are not allowing duplicate album names.
+    public void addAlbum(Album album) {
+        if (!albums.contains(album)) {
+            albums.add(album);
+        }
+    }
+
+    // Method to remove an album from the list of albums.
+    public void removeAlbum(Album album) {
+        albums.remove(album);
+    }
+
+    // Method to rename an album.
+    public void renameAlbum(Album album, String newName) {
+        album.setName(newName);
+    }
+
+    // Album retrieval methods
+
+    // Method to get an album by name.
+    public Album getAlbumByName(String name) {
+        for (Album album : albums) {
+            if (album.getName().equals(name)) {
+                return album;
+            }
+        }
+        return null;
+    }
+
+    // Method to get albums by tag.
+    public List<Album> getAlbumsByTag(Tag tag) {
+        List<Album> matchingAlbums = new ArrayList<>();
+        for (Album album : albums) {
+            for (Photo photo : album.getPhotos()) {
+                if (photo.getTags().contains(tag)) {
+                    matchingAlbums.add(album);
+                    break; // Stop searching this album if a match is found
+                }
+            }
+        }
+        return matchingAlbums;
+    }
+
+    // Method to get albums by date range.
+    public List<Album> getAlbumsByDateRange(java.time.LocalDate startDate, java.time.LocalDate endDate) {
+        List<Album> matchingAlbums = new ArrayList<>();
+        for (Album album : albums) {
+            for (Photo photo : album.getPhotos()) {
+                if (photo.getDate().isAfter(startDate) && photo.getDate().isBefore(endDate)) {
+                    matchingAlbums.add(album);
+                    break; // Stop searching this album if a match is found
+                }
+            }
+        }
+        return matchingAlbums;
+    }
+
+    // Method to get albums by caption.
+    public List<Album> getAlbumsByCaption(String caption) {
+        List<Album> matchingAlbums = new ArrayList<>();
+        for (Album album : albums) {
+            for (Photo photo : album.getPhotos()) {
+                if (photo.getCaption().contains(caption)) {
+                    matchingAlbums.add(album);
+                    break; // Stop searching this album if a match is found
+                }
+            }
+        }
+        return matchingAlbums;
+    }
+
+    // Data persistence methods. Save and load user data. Use serialization and deserialization. Linked to DataManager.
+
+    // Method to save user data.
+    public void saveUserData() {
+        DataManager.saveUserData(this);
+    }
+
+    // Method to load user data.
+    public static User loadUserData(String username) {
+        return DataManager.loadUserData(username);
+    }
+
+    // Equals and hashcode, toString, etc.
+
+    // Equals method.
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User u = (User) o;
+        return username.equals(u.getUsername()) && albums.equals(u.getAlbums());
+    }
+    
+    // Hashcode method.
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(username, albums);
+    }
+
+    // toString method.
+    @Override
+    public String toString() {
+        return "Username: " + username + "\nAlbums: " + albums;
+    }
     
 }
